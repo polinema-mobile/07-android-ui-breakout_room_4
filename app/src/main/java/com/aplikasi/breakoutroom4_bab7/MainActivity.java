a1e1cef7ab946b969e2db117c770900ae175ef92
Fahmi0604 <fahmifirmansyah917@gmail.com>
2020-09-29 11:22:08 +0700

    Initial commit

---
+++ app/src/main/java/com/aplikasi/breakoutroom4_bab7/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aplikasi.breakoutroom4_bab7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
